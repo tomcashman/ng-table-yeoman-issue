c26637b0358f5f3361d91452197dec9817664f87
Thomas Cashman <thomascashman404@gmail.com>
2014-09-12 14:57:28 +0100

    Committing example of issue.

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testAppApp
+ * @description
+ * # testAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngTable'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
